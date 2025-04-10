1857582ce262e3e594458d361db5107022a62d46
Vicenzo Giordana <giordanavicenzo@gmail.com>
2025-04-10 16:02:35 -0300

    Primer commit

---
+++ inefable/src/app/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useEffect } from 'react';
+import Lenis from 'lenis'
+import Intro from '@/components/Intro';
+import Description from '@/components/Description';
+import Section from '@/components/Section';
+import TextParallax from '@/components/TextParallax';
+export default function Home() {
+
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+  return (
+    <main>
+      <Intro />
+      <Description />
+      <Section />
+      {/* <div className='h-screen'></div> */}
+      <TextParallax />
+      {/*text clip mask on scroll*/}
+    </main>
+  );
+}
