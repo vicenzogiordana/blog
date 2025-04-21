5eeca2c1033ac7d39e86ab81ffc62a3b3ae477b9
Vicenzo Giordana <giordanavicenzo@gmail.com>
2025-04-21 20:42:03 -0300

    Arreglo del texto en description

--- inefable/src/components/Description.jsx
+++ inefable/src/components/Description.jsx
@@ -1,22 +1,14 @@
 'use client'
 import { React, useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion';
+import Paragraph from './TextOpacity'
 const phrase = 'The paradoxical blog that explains with words what cannot be explained with words'
 export default function Description() {
-    const container = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: container,
-        offset: ['start end', 'end start']
-    })
+
     return (
-        <div ref={container} className='flex justify-center my-40'>
-            <p className='text-[5.5vw] uppercase text-center max-w-[50vw] leading-none'>
-                {
-                    phrase.split("").map((letter, i) => {
-                        const sm = useTransform(scrollYProgress, [0, 1], [0, Math.floor(Math.random() * -75) - 25])
-                        return <motion.span style={{ top: sm }} key={`l_${i}`} >{letter}</motion.span>
-                    })
-                }
+        <div className='flex justify-center my-40'>
+            <p className='text-[5.5vw] uppercase text-left max-w-[50vw] leading-none'>
+                <Paragraph paragraph={phrase} />
             </p>
         </div>
     )
